8d86d1728d40fe624955ec7d3769aee35672e35f
Cecilia Laborde <cecilia@intellisense.io>
2019-09-29 19:57:19 +0200

    Load test, train, val data

--- classifier1.py
+++ classifier1.py
@@ -1,5 +1,10 @@
 import numpy as np
-import matplotlib.pyplot at plt
+import matplotlib.pyplot as plt
+from utils.load_data import load_data
 
 import tensorflow as tf
 from tensorflow import keras
+
+train_imgs, train_labels = load_data("train")
+val_imgs, val_labels = load_data("valid")
+test_imgs, test_labels = load_data("test")

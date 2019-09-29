633c33b7e037340f05f5b1b7af30a68144582908
Cecilia Laborde <cecilia@intellisense.io>
2019-09-29 19:57:02 +0200

    Load data and labels

--- utils/load_data.py
+++ utils/load_data.py
@@ -7,19 +7,27 @@ import numpy as np
 def load_data(data_type):
     path = os.path.join("data", data_type)
     files = os.listdir(path)
+    materials = ["cardboard", "glass", "metal", "paper", "plastic", "trash"]
+    all_data = []
+    labels = []
     if data_type is "test":
-        all_data = []
-        labels = []
-        materials = ["cardboard", "glass", "metal", "paper", "plastic", "trash"]
         for img in files:
             img_path = os.path.join(path, img)
             all_data.append(image.imread(img_path))
+            ret = -1
+            i = 0
+            while ret == -1:
+                ret = img.find(materials[i])
+                i += 1
+            labels.append(materials[i-1])
 
-            
-    
     if data_type is "train" or data_type is "valid":
-        
-
-
+        for mat in materials:
+            mat_path = os.path.join(path, mat)
+            files = os.listdir(mat_path) 
+            for img in files:
+                img_path = os.path.join(mat_path, img)
+                all_data.append(image.imread(img_path))
+            labels = labels + [mat] * len(files)
     return (np.array(all_data), labels)
         

c4dcae216e93f4c7f8de66905a8b16bd08945092
Cecilia Laborde <cecilialaborde@gmail.com>
2019-10-06 17:46:28 +0200

    Numerical labels

--- utils/load_data.py
+++ utils/load_data.py
@@ -8,6 +8,7 @@ def load_data(data_type):
     path = os.path.join("data", data_type)
     files = os.listdir(path)
     materials = ["cardboard", "glass", "metal", "paper", "plastic", "trash"]
+    mat_to_nr = {"cardboard": 0, "glass": 1, "metal": 2, "paper": 3, "plastic": 4, "trash": 5}
     all_data = []
     labels = []
     if data_type is "test":
@@ -19,7 +20,7 @@ def load_data(data_type):
             while ret == -1:
                 ret = img.find(materials[i])
                 i += 1
-            labels.append(materials[i-1])
+            labels.append(mat_to_nr[materials[i-1]])
 
     if data_type is "train" or data_type is "valid":
         for mat in materials:
@@ -28,6 +29,6 @@ def load_data(data_type):
             for img in files:
                 img_path = os.path.join(mat_path, img)
                 all_data.append(image.imread(img_path))
-            labels = labels + [mat] * len(files)
+            labels = labels + [mat_to_nr[mat]] * len(files)
     return np.array(all_data), labels
         
